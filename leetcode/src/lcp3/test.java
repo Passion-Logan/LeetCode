135cabdbd69324b25310786508ce66b14621976d
passion-logan <wangdadamailbox@163.com>
2019-12-25 15:47:21 +0800

    提交练习

--- leetcode/src/lcp3/test.java
+++ leetcode/src/lcp3/test.java
@@ -61,97 +61,9 @@ public class test {
         String command = "RUUR";
         int[][] obstacles = {{10, 5}, {1, 6}, {6, 10}, {3, 0}, {0, 3}, {0, 10}, {6, 2}};
 
-        Arrays.sort(obstacles, (a, b) -> {
-            if (a[0] == b[0]) {
-                return a[1] - b[1];
-            }
-            return a[0] - b[0];
-        });
-
-        System.out.println(Arrays.deepToString(obstacles));
-
-        int maxX = obstacles.length == 0 ? -1 : obstacles[obstacles.length - 1][0];
-        int maxY = obstacles.length == 0 ? -1 : obstacles[obstacles.length - 1][1];
-        int[][] path = test.line(command, x, y, maxX, maxY);
-
-
-        // 获取全部路线
-//        System.out.println(Arrays.deepToString(path));
-        // 判断是否能安全到达终点
-//        System.out.println(test.security(path, obstacles));
-
         System.out.println(test.robot2(command, obstacles, x, y));
 
         System.out.println(test.robot(command, obstacles, x, y));
-    }
-
-    private int[][] line(String command, int x, int y, int maxX, int maxY) {
-        int i = 0;
-        boolean flag = true;
-
-        char[] data = command.toCharArray();
-        int length = data.length;
-        int[] xs = new int[data.length];
-        int[] ys = new int[data.length];
-
-        if (maxX < 0) {
-            return null;
-        }
-
-        while (flag) {
-            if (i == 0) {
-                xs[i] = data[i] == 'R' ? 1 : 0;
-                ys[i] = data[i] == 'U' ? 1 : 0;
-            } else {
-                xs[i] = data[i] == 'R' ? xs[i - 1] + 1 : xs[i - 1];
-                ys[i] = data[i] == 'U' ? ys[i - 1] + 1 : ys[i - 1];
-            }
-            if ((xs[i] > maxX && ys[i] > maxY) || (xs[i] == x && ys[i] == y)) {
-                int[][] line = new int[i + 1][2];
-                int j = 0;
-                while (j <= i) {
-                    line[j][0] = xs[j];
-                    line[j][1] = ys[j];
-                    j++;
-                }
-//                System.out.println(Arrays.deepToString(line));
-                return line;
-            }
-            i++;
-            // 判断数组扩容或者缩容
-            if (i == length) {
-                xs = Arrays.copyOf(xs, i * 2);
-                ys = Arrays.copyOf(ys, i * 2);
-                int oldLength = data.length;
-                data = Arrays.copyOf(data, data.length * 2);
-                for (int z = 0; z < oldLength; z++) {
-                    data[oldLength + z] = data[z];
-                }
-                length = i * 2;
-            }
-        }
-
-        return null;
-    }
-
-    private boolean security(int[][] path, int[][] obstacles) {
-        if (obstacles.length == 0) {
-            return true;
-        }
-
-        for (int i = 0; i < obstacles.length; i++) {
-            for (int j = 0; j < path.length; j++) {
-                if (path[j][0] > obstacles[i][0] && path[j][1] > obstacles[i][1]) {
-                    continue;
-                }
-                if (path[j][0] == obstacles[i][0] && path[j][1] == obstacles[i][1]) {
-                    System.out.println("[" + obstacles[i][0] + ", " + obstacles[i][1] + "]");
-                    return false;
-                }
-            }
-        }
-
-        return true;
     }
 
     public boolean robot(String command, int[][] obstacles, int x, int y) {
